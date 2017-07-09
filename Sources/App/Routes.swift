c29fb51ba60cc1ac4710b3a2ab972417b2a01542
Issarapong Poesua <dekablade01@gmail.com>
2017-07-10 01:35:42 +0700

    /GET getPeople

--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -1,25 +1,25 @@
+
 import Vapor
 
 extension Droplet {
+    
     func setupRoutes() throws {
-        get("hello") { req in
-            var json = JSON()
-            try json.set("hello", "world")
-            return json
+        get("getPeople") { _ in
+            return try JSON( node: [
+                "status" : true,
+                "result" : [
+                    "people" : [
+                        [
+                        "name" : "Issarapong Poesua",
+                        "age" : 24
+                        ],
+                        [
+                            "name": "Tim Cook",
+                            "age" : 30
+                        ]
+                    ]
+                ]
+            ])
         }
-
-        get("plaintext") { req in
-            return "Hello, world!"
-        }
-
-        // response to requests to /info domain
-        // with a description of the request
-        get("info") { req in
-            return req.description
-        }
-
-        get("description") { req in return req.description }
-        
-        try resource("posts", PostController.self)
     }
 }
